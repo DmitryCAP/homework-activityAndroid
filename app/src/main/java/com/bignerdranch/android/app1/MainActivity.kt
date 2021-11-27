4ae9838ed11fc472841800dccf547ff03336b19e
DmitryCAP <dmitriidrozdov3@gmail.com>
2021-11-27 19:56:04 +0700

    first commit

---
+++ app/src/main/java/com/bignerdranch/android/app1/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.bignerdranch.android.app1
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+const val TAG = "MainActivity"
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Log.d(TAG,"Ты видел деву на скале")
+    }
+
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG,"В одежде белой над волнами")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG,"Когда, бушуя в бурной мгле,")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG,"Играло море с берегами,")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG,"Когда луч молний озарял")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG,"onRestart")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG,"Ее всечасно блеском алым...")
+    }
+}
